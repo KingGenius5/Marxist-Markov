f58357f09db7da6277c4b26b0ebfbe75dd39590d
Muhammad Tifak <moht20009@gmail.com>
2019-12-12 23:43:19 -0800

    Imported sample/rearrange code
    Code seems up to date

---
+++ rearrange.py
@@ -0,0 +1,40 @@
+import sys
+import random	import random as r
+
+
+def flip_words(words_list):
+    for word in words_list:
+
+        for i, char in enumerate(word):
+            if i < len(word)/2:
+                tmp_char = word[i]
+                word[i] = word[len(word) - i - 1]
+                word[len(word) - i - 1] = tmp_char
+
+    return words_list
+
+
+def shuffle_list(words_list):
+    """
+    Shuffles order of words in a given list
+    """
+    for i, word in enumerate(words_list):
+        random_index = r.randint(0, len(words_list) - 1)
+        words_list[i] = words_list[random_index]
+        words_list[random_index] = word
+
+    return words_list
+
+
+if __name__ == "__main__":
+    words_list = []
+
+    #Add args to list
+    for arg in range(1, len(sys.argv)):
+        words_list.append(sys.argv[arg])
+
+    print(words_list)
+
+    words_list = shuffle_list(words_list)
+
+    print(words_list)
---
+++ sample.py
@@ -0,0 +1,65 @@
+import random
+import sys
+
+from histogram import read_file, histogram_dictonary
+
+
+
+#create the sample list
+
+
+def better_words(count, token_count, histogram):
+
+    #get total number of words from selection
+    sentence = ""
+    while count > 0:
+        #choose a random number in that range.
+        rand_value = random.randint(0, token_count - 1)
+        #keeps track of value
+        total_count = 0
+
+        #loop through list and add values to total count
+        for key, value in histogram.items():
+
+            if rand_value <= total_count:
+                sentence += f" {key}"
+                break
+
+            total_count += value
+        #decrement count
+        count -= 1
+
+    return sentence
+
+
+
+#choose word from list a certain count of times
+def choose_words(count, words_list):
+    count_list = list()
+    count_histogram = dict()
+
+    for i in range(count):
+        count_list.append(random.choice(words_list))
+
+    #add the list of word counts to dictonary
+    for word in count_list:
+        count_histogram[word] = count_histogram.get(word, 0) + 1
+
+    return count_histogram
+
+
+
+
+if __name__ == "__main__":
+
+    file_name = sys.argv[1]
+
+    words = read_file(file_name)
+
+    token_count = len(words)
+
+    hist = histogram_dictonary(words)
+
+    sentence = better_words(10, token_count, hist)
+
+    print(sentence)
